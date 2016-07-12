8804493ce0ce156e165fe7d444f7f6d4d6f17541
eronemus <emily.ronemus@gmail.com>
2016-07-12 14:05:42 -0400

    init push

---
+++ Portfolio2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portfolio2016.Startup))]
+namespace Portfolio2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
